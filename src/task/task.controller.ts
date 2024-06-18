13ae2cfd6c029c67b60a9ea0492fbdcfdd637639
Matheus Martins <matheusmmartins8@gmail.com>
2024-06-18 11:33:02 -0300

    feat: implementa módulo de banco de dados | refatora serviços utilizando typeorm

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { FindAllParameters, TaskDto } from './task.dto';
+import { FindAllParameters, TaskDto, TaskRouteParameters } from './task.dto';
 import { TaskService } from './task.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 
@@ -19,23 +19,23 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  create(@Body() task: TaskDto) {
-    this.taskService.create(task);
+  async create(@Body() task: TaskDto): Promise<TaskDto> {
+    return await this.taskService.create(task);
   }
 
   @Get('/:id')
-  findById(@Param('id') id: string): TaskDto {
-    return this.taskService.findById(id);
+  async findById(@Param('id') id: string): Promise<TaskDto> {
+    return await this.taskService.findById(id);
   }
 
   @Get()
-  findAll(@Query() params: FindAllParameters): TaskDto[] {
-    return this.taskService.findAll(params);
+  async findAll(@Query() params: FindAllParameters): Promise<TaskDto[]> {
+    return await this.taskService.findAll(params);
   }
 
-  @Put()
-  update(@Body() task: TaskDto) {
-    return this.taskService.update(task);
+  @Put('/:id')
+  async update(@Param() params: TaskRouteParameters, @Body() task: TaskDto) {
+    return await this.taskService.update(params.id, task);
   }
 
   @Delete('/:id')
