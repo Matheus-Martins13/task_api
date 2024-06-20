c168c2b74116679c2ab4f438363611aec1b7e6ec
Matheus Martins <matheusmmartins8@gmail.com>
2024-06-20 16:10:08 -0300

    feat: cria dois endpoints para Users: 'delete' e 'findByUserName' utilizando authguard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserDto } from './user.dto';
-// import { AuthGuard } from 'src/auth/auth.guard';
+import { UserDto, UserRouteParameters } from './user.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
   @Post()
   create(@Body() user: UserDto) {
     this.usersService.create(user);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/:username')
+  async findByUserName(@Param() params: UserRouteParameters): Promise<UserDto> {
+    return await this.usersService.findByUserName(params.username);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete('/:id')
+  async remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
   }
 }
